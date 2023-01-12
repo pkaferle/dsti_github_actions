394b40a4ba0223db828bc1480f747eca3cbc0fb8
pkaferle <62647072+pkaferle@users.noreply.github.com>
2023-01-12 14:58:02 +0100

    Update test_first_function.py

--- tests/test_first_function.py
+++ tests/test_first_function.py
@@ -4,7 +4,7 @@ from src.user_functions import *
 
 def test_email_with_user_input_no_at_sign(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
-    assert get_email_from_input() == 'petra.adaltas.com'
+    assert get_email_from_input() is None
 
 def test_email_with_user_input_no_dot(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra@adaltascom'))

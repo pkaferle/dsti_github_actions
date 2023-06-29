fe6afc6059fc64bb1730c1fbb0a06b2229ce7105
pkaferle <62647072+pkaferle@users.noreply.github.com>
2023-06-29 16:10:01 +0200

    dummy commit test_first_function.py

--- tests/test_first_function.py
+++ tests/test_first_function.py
@@ -2,6 +2,8 @@ import pytest
 import io
 from src.user_functions import *
 
+
+
 def test_email_with_user_input_no_at_sign(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
     assert get_email_from_input() is None
